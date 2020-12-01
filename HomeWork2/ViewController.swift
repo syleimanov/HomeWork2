d2de2ef03b591b322a920ee6b409134608d841d3
VLADIMIR SYLEIMANOV <bannabels@gmail.com>
2020-12-01 13:55:50 +0300

    Initial Commit

---
+++ HomeWork2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2
+//
+//  Created by Vladimir Syleimanov on 01.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HomeWork2Tests/HomeWork2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  HomeWork2Tests.swift
+//  HomeWork2Tests
+//
+//  Created by Vladimir Syleimanov on 01.12.2020.
+//
+
+import XCTest
+@testable import HomeWork2
+
+class HomeWork2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

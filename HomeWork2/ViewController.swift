d302bf8eb0fcf9908e6cbc37f241f9159dd0f2e8
VLADIMIR SYLEIMANOV <bannabels@gmail.com>
2020-12-01 22:25:22 +0300

    промежуточный вариант второй домашки

--- HomeWork2/ViewController.swift
+++ HomeWork2/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redColor: UIView!
+    @IBOutlet var yellowColor: UIView!
+    @IBOutlet var greenColor: UIView!
+    
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        redColor.alpha = 0.3
+        yellowColor.alpha = 0.3
+        greenColor.alpha = 0.3
+        toggleButton.layer.cornerRadius = 8
+        redColor.layer.cornerRadius = 65
+        yellowColor.layer.cornerRadius = 65
+        greenColor.layer.cornerRadius = 65
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapOnButton() {
+        if toggleButton != nil {
+            redColor.alpha = 1
+            toggleButton.setTitle("Next", for: .normal)
+        } else {
+            if redColor.alpha == 1 {
+                redColor.alpha = 0.3
+                yellowColor.alpha = 1
+            }
+      }
+        
+    }
 }
+
+
+
+
 
